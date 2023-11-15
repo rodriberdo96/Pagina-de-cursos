7270815399e5c494c21135d85b1c661de96aa514
Rodri Berdomas <rodriberdomas@gmail.com>
2023-11-15 12:23:15 -0300

    arreglar js

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
-// Definir los usuarios
+
+
+
+$(document).ready(function(){
+
+
 const existingUsers = [
     { username: "rodri1", password: "password1" },
     { username: "rodri2", password: "password2" },
     { username: "rodri3", password: "password3" }
 ];
 
-// conseguir el form de login
-const form = $("form");
-form.submit(function(event) {
-    event.preventDefault(); // Prevent the form from submitting
 
-    // Get the username and password input values
+var boton = $('#btn');
+
+boton.click(function(event){
+
+    event.preventDefault();
+
     const usernameInput = $("#username");
     const passwordInput = $("#password");
     const username = usernameInput.val();
     const password = passwordInput.val();
 
-    // chequear si el usuario existe
     const user = existingUsers.find(user => user.username === username && user.password === password);
     if (user) {
-        // si el usuario existe, guardar el usuario en el local storage
         console.log("Logged in as", user.username);
-        // redirigir a la pagina de progreso
         window.location.href = "progress.html";
     } else {
-        // si el usuario no existe, mostrar un error
         const error = $("#error");
         error.text("Invalid username or password");
     }
 });
 
-// agregar un nuevo usuario
 const registerForm = $("#register-form");
+
 registerForm.submit(function(event) {
     event.preventDefault(); 
 
@@ -56,7 +58,5 @@ registerForm.submit(function(event) {
     // redirigir a la pagina de login
     window.location.href = "login.html";
 });
-
-
-
-
+    
+    });
